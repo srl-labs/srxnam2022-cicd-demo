e72c424ff4325cb390c3b12ca058dda24abbedf1
Karim Radhouani <medkarimrdi@gmail.com>
2022-09-07 10:18:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# SReXperts NAM 2022 CI/CD demo
+
+This repo contains the [Containerlab](https://containerlab.srlinux.dev/) advanced example presented at NAM SReXperts 2022.
+
+Here you can find the files needed to deploy the below topology
+
+![srx2022-Page-1](https://user-images.githubusercontent.com/12892894/138223207-b4ad2ffd-57b7-48d1-8dbe-f5d16f11d151.png)
+
+## Prerequisites
+
+- Install [Docker](https://docs.docker.com/engine/install/)
+- Install [Containerlab](https://containerlab.srlinux.dev/install/)
+- Install [gNMIc](https://gnmic.kmrd.dev/install/)
+
+## Deploy on local machine
+
+Run:
+
+```bash
+bash run.sh
+```
+
+## Deploy via Github Actions
+
+The repo includes a github [workflow](https://github.com/srl-labs/clab-srx2022-advanced/blob/main/.github/workflows/wf.yaml) that deploys the lab on a self-hosted runner.
+
+![lab-gh-actions](https://user-images.githubusercontent.com/12892894/138225607-35f3bad3-5e05-4f37-a74f-f0840029cf0c.png)
---
+++ run.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -e
+
+gnmic_cmd="gnmic --tls-ca clab-srx2022/ca/root/root-ca.pem -u admin -p admin"
+
+printf "gnmic version : $(gnmic version | grep version | awk '{print $NF}')\n"
+printf "clab version  : $(sudo containerlab version | grep version | awk '{print $NF}')\n"
+
+# deploy the lab.
+# if you have limited compute resources, limit the number of workers by
+# uncommenting the `--max-workers 4` flag
+sudo clab deploy -t srx2022.clab.gotmpl --reconfigure #--max-workers 4
+
+# get SR Linux Nodes names and join them with a comma
+srl_nodes=$(docker ps -f label=containerlab=srx2022 -f label=clab-node-kind=srl --format {{.Names}} | paste -s -d, -)
+
+# configure interfaces via gNMI 
+${gnmic_cmd} \
+      --log \
+      -a ${srl_nodes} \
+      -e json_ietf \
+      set \
+      --request-file config/1.interfaces/interfaces_template.gotmpl \
+      --request-file config/1.interfaces/subinterfaces_template.gotmpl \
+      --request-vars config/1.interfaces/interfaces_template_vars.yaml
+
+# configure routing policy via gNMI 
+${gnmic_cmd} \
+      --log \
+      -a ${srl_nodes} \
+      -e json_ietf \
+      set \
+      --request-file config/2.routing-policy/routing_policy_prefix_set_template.gotmpl \
+      --request-file config/2.routing-policy/routing_policy_policy_template.gotmpl \
+      --request-vars config/2.routing-policy/routing_policy_vars.yaml
+
+
+# # configure network-instance via gNMI 
+${gnmic_cmd} \
+      --log \
+      -a ${srl_nodes} \
+      -e json_ietf \
+      set \
+      --request-file config/3.network-instance/network_instance_template.gotmpl \
+      --request-file config/3.network-instance/network_instance_bgp_template.gotmpl \
+      --request-vars config/3.network-instance/network_instance_template_vars.yaml
+
+printf "\n"
+printf "$(date): Waiting 5s before checking nodes BGP status\n"
+sleep 5
+
+for node in $(docker ps -f label=clab-node-kind=srl -f label=containerlab=srx2022 --format {{.Names}})
+do 
+  bgp_state=$(${gnmic_cmd} \
+                  -a $node \
+                  -e ascii \
+                  --format flat \
+                  get \
+                  --path /network-instance[name=default]/protocols/bgp/oper-state | awk '{print $NF}')
+  
+  
+  if [ $bgp_state != "up" ]; then
+    printf "unexpected BGP state for node $node : ${bgp_state^^} (ノ ゜Д゜)ノ ︵ ┻━┻\n"
+    exit 1
+  fi
+  printf "$(date): \\U1F44D $node BGP state is ${bgp_state^^} \n"
+done
+
+printf "\n"
+printf "$(date): Waiting 60s for BGP sessions to be established\n"
+sleep 60
+
+for node in $(docker ps -f label=clab-node-kind=srl -f label=containerlab=srx2022 -f label=clab-node-group=spines --format {{.Names}})
+do
+num_neighbors=$(
+      gnmic -a $node \
+            --tls-ca clab-srx2022/ca/root/root-ca.pem \
+            -u admin \
+            -p admin \
+            -e ascii \
+            --format flat \
+            get \
+            --path /network-instance[name=default]/protocols/bgp/neighbor/ipv4-unicast/oper-state | grep neighbor | wc -l
+            )
+      if [ "$num_neighbors" -ne "6" ]
+      then
+        printf "unexpected number of neighbors for node $node : $num_neighbors (ノ ゜Д゜)ノ ︵ ┻━┻\n"
+        exit 1
+      fi
+      printf "$(date): \\U1F44D $node has $num_neighbors BGP neighbors \n"
+done
